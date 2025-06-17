709aeb5d48a7ecaa26ea7826271daca5ed32d439
Bramvzw <55138203+Bramvzw@users.noreply.github.com>
2025-06-17 21:24:04 +0200

    Register tasks as a module

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,17 @@
 <?php
 
-use Modules\Spotify\Providers\EventServiceProvider;
-use Modules\Spotify\Providers\RouteServiceProvider;
+use Modules\Spotify\Providers\EventServiceProvider as SpotifyEventServiceProvider;
+use Modules\Spotify\Providers\RouteServiceProvider as SpotifyRouteServiceProvider;
 use Modules\Spotify\Providers\SpotifyServiceProvider;
+use Modules\Tasks\Providers\EventServiceProvider as TasksEventServiceProvider;
+use Modules\Tasks\Providers\RouteServiceProvider as TasksRouteServiceProvider;
+use Modules\Tasks\Providers\TasksServiceProvider;
 
 return [
     SpotifyServiceProvider::class,
-    EventServiceProvider::class,
-    RouteServiceProvider::class
+    SpotifyEventServiceProvider::class,
+    SpotifyRouteServiceProvider::class,
+    TasksServiceProvider::class,
+    TasksEventServiceProvider::class,
+    TasksRouteServiceProvider::class
 ];
