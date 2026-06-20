94e0bb1d538fbf94795759ba1f701355761bc89a
Bramvzw <bramvanzwolle@sibi.nl>
2026-06-20 13:57:23 +0200

    CHORE: Remove FindHub module

--- Modules/FindHub/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\FindHub\Http\Controllers\FindHubController;
-
-Route::prefix('find-hub')->name('findhub.')->group(function (): void {
-    Route::get('/', [FindHubController::class, 'index'])->name('index');
-});
